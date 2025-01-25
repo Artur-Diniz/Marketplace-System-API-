4d79a45cee938e4081d870568ed78749e0596f92
Artur_Diniz <arturdiniz06@gmail.com>
2025-01-25 19:42:09 -0300

    implementação de CRUD Cliente CNPJ

--- Controllers/Clientes_CNPJController.cs
+++ Controllers/Clientes_CNPJController.cs
@@ -15,5 +15,147 @@ namespace marktplace_sistem.Controllers
         {
             _context = context;
         }
+
+        #region get
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                List<Clientes_Cnpj> clientes_Cnpjs = await _context
+                .TB_CLIENTES_CNPJ.ToListAsync();
+
+                return Ok(clientes_Cnpjs);
+
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetId(int id)
+        {
+            try
+            {
+
+                if (id == 0)
+                    throw new Exception("O ID não pode ser igual Zero.");
+
+                Clientes_Cnpj clientes = await _context
+                .TB_CLIENTES_CNPJ.FirstOrDefaultAsync(c => c.Id == id);
+
+
+                if (clientes == null)
+                    throw new Exception("ID não encontrado.");
+
+                return Ok(clientes);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+
+        [HttpGet("Cnpj/{cnpj}")]
+        public async Task<IActionResult> getCNPJ(string cnpj)
+        {
+            try
+            {
+                if (cnpj == "")
+                    throw new Exception("O cnpj não pode ser igual Zero.");
+
+                Clientes_Cnpj clientes = await _context
+                .TB_CLIENTES_CNPJ.FirstOrDefaultAsync(c => c.Cnpj == cnpj);
+
+
+                if (clientes == null)
+                    throw new Exception("cnpj não encontrado.");
+
+                return Ok(clientes);
+
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        #endregion
+
+        [HttpPost]
+        public async Task<IActionResult> AddCliente(Clientes_Cnpj clientes)
+        {
+            try
+            {
+                await _context.TB_CLIENTES_CNPJ.AddAsync(clientes);
+                await _context.SaveChangesAsync();
+
+                string mensagem = $"Bem-Vindo  {clientes.Nome}";
+
+                return Ok(mensagem);
+
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> PutCliente(Clientes_Cnpj clientes)
+        {
+            try
+            {
+                _context.TB_CLIENTES_CNPJ.Update(clientes);
+
+
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteCliente(int id)
+        {
+            try
+            {
+                if (id == 0)
+                    throw new Exception("O ID não pode ser igual Zero.");
+
+                Clientes_Cnpj clientes = await _context
+                .TB_CLIENTES_CNPJ.FirstOrDefaultAsync(c => c.Id == id);
+
+
+                if (clientes == null)
+                    throw new Exception("ID não encontrado.");
+
+                _context.TB_CLIENTES_CNPJ.Remove(clientes);
+
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                return Ok(linhasAfetadas);
+
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
     }
 }
--- obj/Debug/net8.0/marktplace sistem.AssemblyInfo.cs
+++ obj/Debug/net8.0/marktplace sistem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("marktplace sistem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddab70b8202a4306dd60d30209cf9c38c81b8c42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5be2f06194f21e389a2023addbea0d3c8e5321")]
 [assembly: System.Reflection.AssemblyProductAttribute("marktplace sistem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("marktplace sistem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
